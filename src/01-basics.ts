849c6b41f3d4d837754919aad2ba2245914da862
Dawid Beres <20098868@mail.wit.ie>
2024-09-20 17:48:17 +0200

    Added type-safety to friend data.

---
+++ src/01-basics.ts
@@ -0,0 +1,56 @@
+interface Friend {
+    name: string,
+    phone: string,
+    age: number,
+}
+
+const friend1 = {
+    name: "Micheal Lebowski",
+    phone: "0873534123",
+    age: 25,
+};
+
+const friend2 = {
+    name: "Mike Wazowski",
+    phone: "09812314123",
+    age: 20,
+};
+
+const friends = [friend1, friend2];
+console.log(friends[1]);
+
+//------------------------------------
+
+const colleague1 = {
+    name: "Steven Spielberg",
+    department: "Engineering",
+    contact: {
+        email: "Stevengraham@gmail.com",
+        extension: 121,
+    },
+};
+
+const colleague2 = {
+    name: "George RR martin",
+    department: "Programmer",
+    contact: {
+        email: "GerogeRR@gmail.com",
+        extension: 231,
+    },
+};
+
+const colleague3 = {
+    name: "Danny Devito",
+    department: "Scripture",
+    contact: {
+        email: "DannyDevito@gmail.com",
+        extension: 801,
+    },
+};
+
+const colleagues = {
+    current: [colleague1,colleague2,colleague3],
+    former:[],
+};
+
+console.log(colleagues.current[0]);
